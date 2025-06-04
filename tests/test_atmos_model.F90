d4217e853046069c4e4b6c3d5c20fcdd980120a4
Daniel Sarmiento <42810219+dpsarmie@users.noreply.github.com>
2025-06-04 12:01:34 -0400

    Update test_atmos_model.F90

--- tests/test_atmos_model.F90
+++ tests/test_atmos_model.F90
@@ -1,6 +1,7 @@
 program test_output_hours
   use fv3atm_cap_mod, only: OutputHours_FrequencyInput, OutputHours_ArrayInput
-  use module_fv3_config, only: dt_atmos, output_fh, lflname_fulltime
+  use module_fv3_config, only: dt_atmos, output_fh
+  use module_fv3_io_def, only: lflname_fulltime
   
   implicit none
   
